2f01ddf91053fba43e154908839b9c7afbd1f87f
CharlieMantium <karol.zieniewicz@gmail.com>
2021-03-03 19:31:18 +0100

    Added basic structure

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
+import Layout from './styles/Layout/Layout';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Layout>
+      <App />
+    </Layout>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 reportWebVitals();
